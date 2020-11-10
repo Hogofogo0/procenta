4a4926af05d735b16acc705ccc70f1112a360a48
Hogofogo0 <68776905+Hogofogo0@users.noreply.github.com>
2020-11-10 14:26:26 +0100

    bla,bla

---
+++ procenta.py
@@ -0,0 +1,38 @@
+import math
+
+x = float(input('Co je x: '))
+y = float(input('Co je y: '))
+F = 100
+
+vysledek1 = x/y*F
+vysledek = y/F
+
+pIS = str(vysledek1)
+pIS2 = str(vysledek)
+
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print ('Výseldek: ' + pIS + '%')
+print ('1% je: ' + pIS2)
